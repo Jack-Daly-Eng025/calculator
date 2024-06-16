db5d9e848b04bef4a5d79a5cb97f9f44b255a90b
Jack-Daly-Eng025 <jac.daly.2021@outlook.com>
2024-06-16 17:58:58 +0100

    commit change

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+
+
+//Working through freeCodeCamp tutorial for Rust. 
+
+use std::env::{args, Args};
+
+fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
+  match operator {
+    '+' => first_number + second_number,
+    '-' => first_number - second_number,
+    '/' => first_number / second_number,
+    '*' | 'X' | 'x' => first_number * second_number,
+    _ => panic!("Invalid operator used."),
+  }
+}
+fn output(first_number: f32, operator: char, second_number: f32, result: f32) -> String {
+  format!(
+    "{} {} {} = {}",
+    first_number, operator, second_number, result
+  )
+}
+
+fn main() {
+  let mut args: Args = args();
+
+  let first: String = args.nth(1).unwrap();
+  let operator: char = args.nth(0).unwrap().chars().next().unwrap();
+  let second: String = args.nth(0).unwrap();
+
+  let first_number = first.parse::<f32>().unwrap();
+  let second_number = second.parse::<f32>().unwrap();
+  let result = operate(operator, first_number, second_number);
+
+  println!("{}", output(first_number, operator, second_number, result));
+}
